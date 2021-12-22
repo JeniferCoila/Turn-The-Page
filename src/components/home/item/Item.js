635e8d72126f9fbba5dcf0de1d45eddcdb1edfcd
Jeni <jeniferfcr@gmail.com>
2021-12-22 18:58:59 -0500

    Remove itemcount from item

--- src/components/home/item/Item.js
+++ src/components/home/item/Item.js
@@ -1,25 +1,11 @@
 import './Item.scss';
 import { Link } from 'react-router-dom'
-import React, { useState } from "react";
-import ModalCartConfirmComponent from "../../../components/modal/cart-confirm/ModalCartConfirm";
-import ItemCount from '../item-count/ItemCount';
 
 const img_path = '/assets/img/books/';
-
-const dataInital = {
-    initial:1,
-    stock: 3
-}
 
 
 const Item = (props) => {
     const {imageLink, price, title, author, slug} = props.data;
-  
-    const [goToCart, setGoToCart] = useState(false);
-  
-    const addTocart = (confirm) => {
-      setGoToCart(confirm);
-    }
       
     return (
       <div className="vlp-product-item card"> 
@@ -31,10 +17,6 @@ const Item = (props) => {
                 <span className="vlp-product-item-price">S/{price}</span>
             </div>
           </Link>
-          {!goToCart ? 
-          <ItemCount onConfirm={addTocart} initial={dataInital.initial} stock= {dataInital.stock} data={props.data}
-          ></ItemCount> :
-          <ModalCartConfirmComponent onConfirm={addTocart}/>}
       </div>
     
   );
